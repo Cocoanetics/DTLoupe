a5d119806a2fae0e822ec90c363e3d80c412c39d
Oliver Drobnik <oliver@cocoanetics.com>
2012-02-28 15:44:33 +0000

    Renamed Demo app delegate to avoid clashing with demo app in other projects

---
+++ Demo/Source/LoupeDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DTLoupeAppDelegate.h
+//  DTLoupe
+//
+//  Created by Michael Kaye on 22/06/2011.
+//  Copyright 2011 sendmetospace.co.uk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoupeDemoAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
